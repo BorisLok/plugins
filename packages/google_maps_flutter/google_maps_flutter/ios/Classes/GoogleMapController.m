10d8b9b49289aabc5ed22bf5fdf05d618731b420
boris <boris@velodash.co>
2021-02-17 15:21:07 +0800

    [Fix] Fix iOS crash by observing map frame change only once.
    [Cause] as above.
    
    [Solution] https://github.com/flutter/plugins/pull/3426/commits/690f17ae5e526b62295a18cedb0bf2451dbb4f97

--- packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapController.m
@@ -125,6 +125,8 @@ static dispatch_block_t delayNotifingAnimationCompletedTask;
     if ([circlesToAdd isKindOfClass:[NSArray class]]) {
       [_circlesController addCircles:circlesToAdd];
     }
+
+    [_mapView addObserver:self forKeyPath:@"frame" options:0 context:nil];
   }
   return self;
 }
@@ -180,7 +182,6 @@ static dispatch_block_t delayNotifingAnimationCompletedTask;
 }
 
 - (UIView*)view {
-  [_mapView addObserver:self forKeyPath:@"frame" options:0 context:nil];
   return _mapView;
 }
 
@@ -188,11 +189,6 @@ static dispatch_block_t delayNotifingAnimationCompletedTask;
                       ofObject:(id)object
                         change:(NSDictionary*)change
                        context:(void*)context {
-  if (_cameraDidInitialSetup) {
-    // We only observe the frame for initial setup.
-    [_mapView removeObserver:self forKeyPath:@"frame"];
-    return;
-  }
   if (object == _mapView && [keyPath isEqualToString:@"frame"]) {
     CGRect bounds = _mapView.bounds;
     if (CGRectEqualToRect(bounds, CGRectZero)) {
